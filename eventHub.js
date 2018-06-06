ab6297031ac7fa96c4fdba878556ee4e8c6af949
Victor <1214120123@qq.com>
2018-06-06 19:18:51 +0800

    Update eventHub.js

--- eventHub.js
+++ eventHub.js
@@ -63,32 +63,3 @@
     window.eventHub = eventHub
   }
 })()
-
-
-
-// try it now
-var eventBus = new eventHub
-
-// emit
-function say(what){
-  eventBus.$remove('say')
-  eventBus.$emit('say', what)
-}
-
-function shout(){
-  eventBus.$emit('shout', 'oh my god')
-}
-
-function showMyName(){
-  eventBus.$emit('showMyName', {name: 'victor'})
-  console.log(eventBus)
-}
-
-function destroy(who){
-  eventBus.$destroy(who)
-  console.log(eventBus)
-}
-// listen
-eventBus.$on('shout', msg => alert(msg))
-eventBus.$on('say', msg => alert(msg))
-eventBus.$on('showMyName', msg => alert(msg.name))
