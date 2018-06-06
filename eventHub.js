ced15a10723880f08be66f73847c2e0c464c039b
Victor <1214120123@qq.com>
2018-06-06 18:15:41 +0800

    move example
    move example to readme

--- eventHub.js
+++ eventHub.js
@@ -49,30 +49,3 @@
     window.eventHub = eventHub
   }
 })()
-
-
-
-// try it now
-var eventBus = new eventHub
-
-// emit
-function sayHi(){
-  eventBus.$emit('say', 'hi')
-}
-
-function shout(){
-  eventBus.$emit('shout', 'oh my god')
-}
-
-function showMyName(){
-  eventBus.$emit('showMyName', {name: 'victor'})
-}
-// listen
-eventBus.$on('shout', msg => alert(msg))
-eventBus.$on('say', msg => alert(msg))
-eventBus.$on('showMyName', msg => alert(msg.name))
-
-
-<button onClick='sayHi()'>sayHi</button>
-<button onClick='shout()'>shout</button>
-<button onClick='showMyName()'>showMyName</button>
